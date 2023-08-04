ae643a2e128d242aa82aa473ec625bc996c2486e
exploit40 <cybermentor2021mdp@gmail.com>
2023-08-05 01:10:12 +0530

    test

--- firstprograme/first/main.go
+++ firstprograme/first/main.go
@@ -1,7 +1,7 @@
-package main
+package main //declaring that this is the main file
 
 import "fmt"
 
 func main() {
-	fmt.Println("hello shwet")
+	fmt.Println("hello shwet") //fmt is standard library
 }
